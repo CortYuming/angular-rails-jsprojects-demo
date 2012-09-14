9762fe83c86a4baf2a5d54b5933f81e99f6f7973
Takashi ITO <cortyuming@gmail.com>
2012-09-14 11:16:26 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jsprojects::Application.config.secret_token = 'e97600bfaf9d5956c02595f9f114eeb2f20ea26ccb49fc7c3e15a1d6ac071863cbeba7060850fa83b1d9fe655823ddba1c969b118794b7e0c9df7c78b06fdb2a'
